ba67d21047ac2f35bde647d9a6bc23b0bf8857cb
Andrew Waller <67027415+andrew-waller@users.noreply.github.com>
2021-08-01 21:37:01 -0500

    Add conditional for chapters without sections/text only

--- src/components/TheReader.vue
+++ src/components/TheReader.vue
@@ -8,11 +8,16 @@
           <span class="text-2xl md:text-3xl text-red-700 font-display mr-3">{{ chapter.id }}</span>
           <span class="text-lg md:text-2xl font-display">{{ chapter.title }}</span>
         </div>
-        <div class="flex flex-col px-5">
+        <div v-if="chapter.sections" class="flex flex-col px-5">
           <div v-for="section in chapter.sections" :key="section.id" class="my-2">
             <span class="text-base md:text-lg text-red-700 font-display mr-2">§</span>
             <span class="text-base md:text-lg font-display mr-3">{{ section.id }}</span>
             <p class="text-base md:text-lg font-display text-justify whitespace-pre-wrap leading-relaxed">{{ section.text }}</p>
+          </div>
+        </div>
+        <div v-else class="flex flex-col px-5">
+          <div class="my-2">
+            <p class="text-base md:text-lg font-display text-justify whitespace-pre-wrap leading-relaxed">{{ chapter.text }}</p>
           </div>
         </div>
       </div>
