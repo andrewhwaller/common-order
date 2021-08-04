b55eeaeb648e96e6a8936934f6638c873af60e8c
Andrew Waller <67027415+andrew-waller@users.noreply.github.com>
2021-08-04 14:51:13 -0500

    remove comments

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -20,22 +20,6 @@
           <div class="ribbon bg-red-500 text-sm whitespace-no-wrap px-5 flex">
             <span class="mx-auto text-white">alpha</span>
           </div>
-          <!-- Right dropdown -->
-<!--          <Menu as="div" class="ml-3 relative">-->
-<!--            <div>-->
-<!--              <MenuButton class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">-->
-<!--                <span class="sr-only">Open user menu</span>-->
-<!--                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />-->
-<!--              </MenuButton>-->
-<!--            </div>-->
-<!--            <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">-->
-<!--              <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">-->
-<!--                <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">-->
-<!--                  <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>-->
-<!--                </MenuItem>-->
-<!--              </MenuItems>-->
-<!--            </transition>-->
-<!--          </Menu>-->
         </div>
       </div>
     </div>
