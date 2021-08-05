c1d408d13e13a66af48edcacf86039767d9ae7ae
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2021-08-05 16:55:11 -0500

    Update splash screen and welcome screen

--- src/components/TheWelcomeScreen.vue
+++ src/components/TheWelcomeScreen.vue
@@ -1,29 +1,15 @@
 <template>
   <div class="flex flex-col">
-    <div class="mx-auto max-w-md px-4 xs:max-w-2xl xs:px-6 xs:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
-      <div class="lg:py-24">
-        <h1 class="mb-4 text-3xl tracking-tight font-bold text-sky-900 xs:mt-5 xs:text-4xl lg:mt-6 xl:text-5xl">
-          <span class="block">Welcome</span>
-        </h1>
-        <p class="text-base text-gray-900 xs:text-xl lg:text-lg xl:text-xl">
-          Ready to get started?
-        </p>
-        <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
-          <div class="rounded-md shadow">
-            <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent
-                      text-base font-medium rounded-md text-white bg-sky-800 hover:bg-sky-900 md:py-4
-                      md:text-lg md:px-10">
-              Open the library
-            </a>
-          </div>
-          <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-            <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent
-                      text-base font-medium rounded-md text-blue-gray-600 bg-white hover:bg-gray-50 md:py-4
-                      md:text-lg md:px-10">
-              Learn more
-            </a>
-          </div>
-        </div>
+    <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+      <h1 class="text-2xl md:text-4xl text-red-700 font-semibold font-display mb-5">Welcome to Common Order!</h1>
+      <p class="mt-4 text-lg leading-6 text-gray-900">Common Order is an growing online repository of doctrinal and liturgical resources from the historical Presbyterian tradition.</p>
+      <div class="mx-auto mt-8 flex flex-col sm:flex-row justify-center">
+        <router-link to="/library" class="items-center px-5 py-3 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-sky-800 hover:bg-sky-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
+          Browse the library
+        </router-link>
+        <a href="https://commonorder.org" class="mt-3 sm:ml-3 sm:mt-0 items-center px-5 py-3 border border-gray-200 shadow-sm text-base font-medium rounded-md text-blue-gray-600 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-gray-500">
+          Learn more
+        </a>
       </div>
     </div>
   </div>
