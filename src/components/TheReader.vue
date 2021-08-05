0fbd61872af69dec7b85e26b0037569bcfeb4076
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2021-08-05 17:09:51 -0500

    Update splash screen and welcome screen

--- src/components/TheReader.vue
+++ src/components/TheReader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="markable">
-    <div v-if="currentDocument" class="flex flex-col">
+  <div>
+    <div id="markable" v-if="currentDocument" class="flex flex-col">
       <h1 class="text-2xl md:text-4xl text-red-700 font-display mx-auto mb-5">{{ currentDocument.data.name }}</h1>
       <p class="text-sm md:text-base font-display mx-auto text-justify">{{ currentDocument.data.description }}</p>
       <div v-for="chapter in filteredDocument.data.chapters" :key="chapter.name" class="mt-10">
@@ -23,7 +23,14 @@
       </div>
     </div>
     <div v-else class="flex flex-col">
-      <h1 class="text-2xl md:text-4xl text-red-700 font-display mb-5">Select a text from the library to begin reading.</h1>
+      <router-link to="/library" class="relative block w-full h-1/2 border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+        </svg>
+        <span class="mt-2 block text-sm font-medium text-gray-900">
+          Select a text from the Library to start reading!
+        </span>
+      </router-link>
     </div>
   </div>
 </template>
--- src/components/TheWelcomeScreen.vue
+++ src/components/TheWelcomeScreen.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col">
     <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
       <h1 class="text-2xl md:text-4xl text-red-700 font-semibold font-display mb-5">Welcome to Common Order!</h1>
-      <p class="mt-4 text-lg leading-6 text-gray-900">Common Order is an growing online repository of doctrinal and liturgical resources from the historical Presbyterian tradition.</p>
+      <p class="mt-4 text-lg leading-6 text-gray-900">Common Order is a growing online repository of doctrinal and liturgical resources from the historical Presbyterian tradition.</p>
       <div class="mx-auto mt-8 flex flex-col sm:flex-row justify-center">
         <router-link to="/library" class="items-center px-5 py-3 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-sky-800 hover:bg-sky-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">
           Browse the library
