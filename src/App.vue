0c4dad7f619b66a8213e6e879a254aa76b46f27d
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2021-09-11 13:51:20 -0500

    Rearrange Reader.vue to allow for hiding search input; add logic/listeners to show/hide search on scroll; remove alpha ribbon in App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,10 @@
       <TheMobileSidebar />
       <TheDesktopSidebar />
       <div class="flex flex-col w-0 flex-1 h-full overflow-hidden">
-        <main id="main" class="flex-1 relative overflow-y-auto focus:outline-none sm:py-20 pb-32 pt-20">
-            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-              <div class="max-w-3xl mx-auto px-5 sm:px-0">
-                <router-view />
-              </div>
-            </div>
-          <div class="ml-4 flex items-center md:ml-6">
-            <div class="ribbon bg-red-500 text-sm whitespace-no-wrap px-5 flex">
-              <span class="mx-auto text-white">alpha</span>
+        <main id="main" class="flex-1 relative overflow-y-auto focus:outline-none">
+          <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div class="max-w-3xl mx-auto px-5 sm:px-0">
+              <router-view />
             </div>
           </div>
         </main>
@@ -60,16 +55,5 @@ export default {
 </script>
 
 <style scoped>
-.ribbon {
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 94px;
-  height: 21px;
-  margin-right: -22px;
-  margin-top: 15px;
-  -ms-transform: rotate(45deg); /* IE 9 */
-  -webkit-transform: rotate(45deg); /* Safari prior 9.0 */
-  transform: rotate(45deg); /* Standard syntax */
-}
+
 </style>
