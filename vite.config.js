df1714c11a5c300738b3b9f2fa1b5044d09c35a7
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2022-01-23 14:20:00 -0600

    Update vite config with manifest/PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,37 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Common Order',
+        short_name: 'Common Order',
+        description: 'Common Order is an online repository of doctrinal and liturgical resources from the historical Presbyterian tradition.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
