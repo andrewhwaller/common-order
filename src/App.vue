853b36d0c3ffff0fa9e2a075d21dbbacd35183ef
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2022-01-26 12:06:20 -0600

    Add keep-alive wrapper for router-view

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,9 @@
         <main id="main" class="flex-1 relative overflow-y-auto focus:outline-none">
           <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div class="max-w-3xl mx-auto px-5 sm:px-0">
-              <router-view />
+              <keep-alive>
+                <router-view />
+              </keep-alive>
             </div>
           </div>
         </main>
